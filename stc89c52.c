b8ab0b373be971c2fe58161cb921f4cf1845f8e6
Ken Yap <retiredfeline@outlook.com>
2024-01-25 19:06:48 +1100

    Modify code so that it can be retargetted to another MCU more easily

---
+++ stc89c52.c
@@ -0,0 +1,59 @@
+#include "clock.h"
+
+void timer0(void) __interrupt(TF0_VECTOR)
+{
+	tickdiv--;		// TL0 will reload from TH0
+}
+
+void ports_init(void)
+{
+#ifndef	LOWON
+	// set SEGMENTS port to push-pull
+	SEGMENTS_M0 = 0xFF;
+#endif
+}
+
+void timer_init(void)
+{
+	TMOD = T0_M1 | T1_M0;	// mode 2 on T0, mode 1 on T1
+	TH0 = COUNT_TL0;	// load recurring divisor
+	TL0 = COUNT_TL0;	// overflow next cycle
+	ET0 = 1;		// enable T0 interrupts
+	TR0 = 1;		// turn on T0
+	EA = 1;			// enable global interrupts
+}
+
+void pullup_init(void)
+{
+#ifdef	EXT_PULLUP
+	P1M0 = 0x0A;		// P1M0.2,3 = 1, open drain
+	P1M1 = 0x0A;		// P1M1.2,3 = 1, open drain
+#endif
+}
+
+#ifdef	TIMER_DELAY
+
+#define	SCANDIV		(65536-5)	// 5 us at least
+
+void delay5us() __naked
+{
+	TH1 = SCANDIV >> 8;
+	TL1 = SCANDIV & 0xff;
+	TR1 = 1;		// start T1
+	while (TF1 == 0)
+		;		// wait for overflow
+	TR1 = 0;		// turn off T1
+	TF1 = 0;		// clear overflow
+	__asm__("ret");
+}
+
+#else
+
+void delay5us() __naked
+{
+	// call and ret take 2 us each, add a nop for 5 us total
+	// add more nops if crystal > 12 MHz
+	__asm__("nop\nnop\nret");
+}
+
+#endif	// TIMER_DELAY
